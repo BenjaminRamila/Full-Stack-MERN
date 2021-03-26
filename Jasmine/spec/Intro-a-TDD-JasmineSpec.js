c4bf113e941a0d21a86d18944009da289f4bcd62
Benjamin Ramila <benjaminramila@gmail.com>
2021-03-25 23:48:49 -0300

    added a new TDD item

--- Jasmine/spec/Intro-a-TDD-JasmineSpec.js
+++ Jasmine/spec/Intro-a-TDD-JasmineSpec.js
@@ -13,6 +13,12 @@ function sumFirstLast(arr){
     return sum; 
 }
     
+function sumMaxMin(arr){
+    var sum = Math.max(...arr) + Math.min(...arr);
+    return sum;
+
+}
+
 describe("Sum1toN", function() { 
     it("should return 3 when we pass 2 as an argument", function() { 
         expect(Sum1toN(2)).toEqual(3); 
@@ -34,5 +40,16 @@ describe("sumFirstLast", function() {
     }); 
     it("should return -10 when we pass [-6,23,3,-4] as an argument", function() { 
         expect(sumFirstLast([-6,23,3,-4])).toEqual(-10); 
+    }); 
+});
+describe("sumMaxMin", function() { 
+    it("should return 6 when we pass [1,2,5] as an argument", function() { 
+        expect(sumMaxMin([1,2,5])).toEqual(6); 
+    }); 
+    it("should return 35 when we pass [2,33,8] as an argument", function() { 
+        expect(sumMaxMin([2,33,8])).toEqual(35); 
+    }); 
+    it("should return 17 when we pass [-6,23,3,-4] as an argument", function() { 
+        expect(sumMaxMin([-6,23,3,-4])).toEqual(17); 
     }); 
 });
