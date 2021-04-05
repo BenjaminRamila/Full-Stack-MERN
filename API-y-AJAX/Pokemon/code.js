08021e1143282f92834a068a4dc206b2cd0ffa38
Benjamin Ramila <benjaminramila@gmail.com>
2021-04-05 16:09:19 -0400

    first commit this 151 awesome pokemons

---
+++ API-y-AJAX/Pokemon/code.js
@@ -0,0 +1,9 @@
+var repetitions = 151;
+
+for (var i = 1; i <= repetitions; i++) {
+  document.body.innerHTML +=
+    "<img src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+    i +
+    ".png>";
+  
+}
