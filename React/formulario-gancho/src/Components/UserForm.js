9d93c35b3f4fc27a1f9907d0d0fe60fcc9575c19
Benjamin Ramila <benjaminramila@gmail.com>
2021-04-17 23:37:58 -0400

    simplified onChange function for scalability

--- React/formulario-gancho/src/Components/UserForm.js
+++ React/formulario-gancho/src/Components/UserForm.js
@@ -3,124 +3,60 @@ import React, { useState } from "react";
 const UserForm = (props) => {
   const { inputs, setInputs } = props;
 
-  const [firstName, setFirstName] = useState("");
-  const [firstNameError, setFirstNameError] = useState("");
-
-  const [lastName, setLastName] = useState("");
-  const [lastNameError, setLastNameError] = useState("");
-
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [confirmPasswordError, setConfirmPasswordError] = useState("");
-
-  const handleFirstName = (e) => {
+  const handleOnChange = (e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
-    setFirstName(e.target.value);
-    if (e.target.value.length < 1) {
-      setFirstNameError(" is required!");
-    } else if (e.target.value.length < 3) {
-      setFirstNameError(" must be 3 characters or longer!");
-    } else if (e.target.value.length > 2) {
-      setFirstNameError("");
-    }
-  };
-
-  const handleLastName = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-    setLastName(e.target.value);
-    if (e.target.value.length < 1) {
-      setLastNameError(" is required!");
-    } else if (e.target.value.length < 3) {
-      setLastNameError(" must be 3 characters or longer!");
-    } else if (e.target.value.length > 2) {
-      setLastNameError("");
-    }
-  };
-
-  const handleEmail = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-    setEmail(e.target.value);
-    if (e.target.value.length < 1) {
-      setEmailError(" is required!");
-    } else if (e.target.value.length < 3) {
-      setEmailError(" must be 3 characters or longer!");
-    } else if (e.target.value.length > 2) {
-      setEmailError("");
-    }
-  };
-
-  const handlePassword = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-    setPassword(e.target.value);
-    if (e.target.value.length < 1) {
-      setPasswordError(" is required!");
-    } else if (e.target.value.length < 8) {
-      setPasswordError(" must be 8 characters or longer!");
-    } else if (e.target.value.length > 2) {
-      setPasswordError("");
-    }
-  };
-
-  const handleConfirmPassword = (e) => {
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-    setConfirmPassword(e.target.value);
-    if (e.target.value !== password) {
-      setConfirmPasswordError(" does not match!");
-  
-    } else {
-      setConfirmPasswordError("");
-    }
-
   };
 
   return (
-    <form>
+    <form onChange={handleOnChange}>
       <div className="formBox">
         <label>First Name </label>
-        <input type="text" name="firstName" onChange={handleFirstName} />
-        {firstNameError ? (
-          <p style={{ color: "red" }}>First Name{firstNameError}</p>
+        <input type="text" name="firstName" />
+        {inputs.firstName.length < 3 && inputs.firstName.length !== 0 ? (
+          <p style={{ color: "red" }}>
+            First Name must be longer than 3 characters
+          </p>
         ) : (
           ""
         )}
       </div>
       <div className="formBox">
         <label>Last Name </label>
-        <input type="text" name="lastName" onChange={handleLastName} />
-        {lastNameError ? (
-          <p style={{ color: "red" }}>Last Name{lastNameError}</p>
+        <input type="text" name="lastName" />
+        {inputs.lastName.length < 3 && inputs.lastName.length !== 0 ? (
+          <p style={{ color: "red" }}>
+            Last Name must be longer than 3 characters
+          </p>
         ) : (
           ""
         )}
       </div>
       <div className="formBox">
         <label>Email Address </label>
-        <input type="text" name="email" onChange={handleEmail} />
-        {emailError ? (
-          <p style={{ color: "red" }}>Email{emailError}</p>
+        <input type="text" name="email" />
+        {inputs.email.length < 3 && inputs.email.length !== 0 ? (
+          <p style={{ color: "red" }}>Email must be longer than 3 characters</p>
         ) : (
           ""
         )}
       </div>
       <div className="formBox">
         <label>Password </label>
-        <input type="password" name="password" onChange={handlePassword} />
-        {passwordError ? (
-          <p style={{ color: "red" }}>Password{passwordError}</p>
+        <input type="password" name="password" />
+        {inputs.password.length < 3 && inputs.password.length !== 0 ? (
+          <p style={{ color: "red" }}>
+            Your password must be at least 8 characters long
+          </p>
         ) : (
           ""
         )}
       </div>
       <div className="formBox">
         <label>Confirm Password </label>
-        <input type="password" name="confirmPassword" onChange={handleConfirmPassword} />
-        {confirmPasswordError ? (
-          <p style={{ color: "red" }}>Password{confirmPasswordError}</p>
+        <input type="password" name="confirmPassword" />
+        {inputs.confirmPassword !== inputs.password &&
+        inputs.confirmPassword.length !== 0 ? (
+          <p style={{ color: "red" }}>Your password doesn't match!</p>
         ) : (
           ""
         )}
